20a870ad43c2aa071042ffe7e46ee6c0264a115a
Carlos Daniel Corrales <carlox59681@gmail.com>
2024-09-17 10:23:11 -0500

    se agrega nueva query por parte de Carlos Daniel codigo 2122878

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const typeDefs = gql`
   type Query {
     hello(message: String!): String
+    helloCarlos(message: String!): String
   }
 `;
 
@@ -14,6 +15,9 @@ const resolvers = {
   Query: {
     hello: (_, { message }) => {
         return `¡Hola, ${message}! Un saludo por parte del profe `;
+      },
+    helloCarlos: (_, { message }) => {
+        return `¡Hola, ${message}! Un saludo por parte de Carlos Daniel`;
       },
   },
 };
